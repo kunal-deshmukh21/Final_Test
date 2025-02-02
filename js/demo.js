3f78f0f0a8814fec7269701b8c27a2c74dcd6e00
kunal-deshmukh21 <kunaldeshmukh192@gmail.com>
2025-02-02 12:46:14 +0530

    change made

--- js/demo.js
+++
@@ -1,6 +0,0 @@
-import { initializeForm } from './handlers/formHandler.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('demoForm');
-    initializeForm(form, 'demo');
-});
--- js/exchange.js
+++
@@ -1,6 +0,0 @@
-import { initializeForm } from './handlers/formHandler.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('exchangeForm');
-    initializeForm(form, 'exchange');
-});
